f0c5875fc6e72bcbd128ece0e8fc8fc14d7b1b41
LaminarDynamics <LaminarDynamics@gmail.com>
2022-09-26 22:36:19 -0400

    Intercept logic in progress

--- logic.js
+++ logic.js
@@ -34,16 +34,38 @@ function MoveRays(speed) {  // Adjust line ends to simulate movement
         if (ray.start[0] < 0 || ray.start[0] > my_width || ray.start[1] < 0 || ray.start[1] > my_height) {  // Check if rays exiting canvas and remove them for performance
             listOfRays.splice(listOfRays.indexOf(ray), 1);
         }
+
+        CheckIntersections(ray);
     });
 }
 
 
 listOfRays = [];
+listOfWalls = [
+    [my_width * .75, my_height * .25, my_width * .75, my_height * .75],
+    [my_width * .75, my_height * .75, my_width * .25, my_height * .75]
+];
+// console.log(listOfWalls)
 // CreateRays();
 
 function mouseClicked() {
     let click_location = [mouseX, mouseY];
     CreateRays(click_location);
+}
+
+function DrawPermanents() {
+    stroke(0, 255, 50);
+    strokeWeight(1);
+    listOfWalls.forEach(wall => {
+        line(wall[0], wall[1], wall[2], wall[3]);
+    });
+}
+
+
+function CheckIntersections(current_ray) {
+    // let rayToCheck = CreateLine([current_ray[0], current_ray[1]], [current_ray[2], current_ray[3]]);
+    // console.log(rayToCheck)
+
 }
 
 
@@ -52,14 +74,21 @@ function draw() {
     clear();
     background(0);
 
+    DrawPermanents();
+
+
     stroke(255, 5, 0);
-    strokeWeight(.5)
+    strokeWeight(.5);
     MoveRays(1);
 
 
-    console.log("Number of rays = ", listOfRays.length)
-    console.log(touches)
+    // console.log("Number of rays = ", listOfRays.length)
+    // console.log(touches)
 }
+
+
+
+
 
 function showCoordinates(event) {   // For touchscreens
     var touch_x = Math.round(event.touches[0].clientX);
--- maths.js
+++ maths.js
@@ -12,3 +12,25 @@ function VectorToCartesian(magnatude, direction) {
     let y = magnatude * Math.sin((direction * Math.PI) / 180.0)
     return [x, y]
 }
+
+
+// function CreateLine(p1, p2) {
+//     let A = (p1[1] - p2[1])
+//     let B = (p2[0] - p1[0])
+//     let C = (p1[0] * p2[1] - p2[0] * p1[1])
+//     return [A, B, -C]
+// }
+
+// function intersection(L1, L2) {
+//     let D = L1[0] * L2[1] - L1[1] * L2[0]
+//     let Dx = L1[2] * L2[1] - L1[1] * L2[2]
+//     let Dy = L1[0] * L2[2] - L1[2] * L2[0]
+//     if (D != 0) {
+//         let x = Dx / D
+//         let y = Dy / D
+//         return [x, y]
+//     }
+//     else {
+//         return False
+//     }
+// }
